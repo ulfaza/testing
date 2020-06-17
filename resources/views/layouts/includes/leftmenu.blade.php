5a869e7e2d88e092addd2ea49fdd08c456ee2aa1
ulfaza <ulfatuzzahroh2@gmail.com>
2020-06-17 18:54:45 +0700

    frontend tambah aplikasi

--- resources/views/layouts/includes/leftmenu.blade.php
+++ resources/views/layouts/includes/leftmenu.blade.php
@@ -6,8 +6,14 @@
         <ul class="nav nav-list">
             <li><div class="left-bg"></div></li>
             <li class="time">
-              <h1 class="animated fadeInLeft">21:00</h1>
-              <p class="animated fadeInRight">Sat,October 1st 2029</p>
+              <center>
+              <h3 id="time" class="animated fadeInLeft">21:00</h3>
+              <p class="animated fadeInRight">
+                <?php
+                  echo date("l, M d Y");
+                ?> 
+              </p>
+            </center>
             </li>
             <li class="active ripple">
               <a class="tree-toggle nav-header" href="/home">
--- resources/views/uji.blade.php
+++ resources/views/uji.blade.php
@@ -3,42 +3,65 @@
 
 @section('content')
 
-<!-- start: Content -->
-            <div id="content">
-                <div class="panel box-shadow-none content-header">
-                  <div class="panel-body">
-                    <div class="col-md-12">
-                        <h3 class="animated fadeInLeft">UJI SOFTWARE</h3>
-                        <!--<p class="animated fadeInDown">
-                          Form <span class="fa-angle-right fa"></span> Form Element
-                        </p>-->
-                    </div>
+<style>
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 80%;
+  margin-left: 10%;
+  margin-bottom: 105px;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #5DADE2;
+  color: white;
+}
+
+</style>
+
+<div id="content">
+  <div class="panel box-shadow-none content-header">
+     <div class="panel-body">
+       <div class="col-md-12">
+           <h3 class="animated fadeInLeft">Uji Aplikasi</h3>
+           <p class="animated fadeInDown">
+        </p>
+      </div>
+    </div>
+  </div>
+
+ <div class="col-md-12 top-20 padding-0">
+    <div class="col-md-12">
+      <div class="panel">
+        <div class="panel-heading"><h3>Spesifikasi Aplikasi</h3></div>
+          <div class="panel-body">
+            <div class="responsive-table">
+              <form form action="/storeaplikasi" method="post" class="form-horizontal">
+              {{ csrf_field() }}
+               <div class="panel-body" style="padding-bottom:30px;">
+                <div class="col-md-12">
+
+                  <div class="form-group"><label class="col-sm-2 control-label text-left">Nama Aplikasi</label>
+                    <div class="col-sm-10"><input type="text" class="form-control"></div>
                   </div>
                 </div>
-
-                <div class="form-element">
-                  <div class="col-md-12 padding-0">
-                    <div class="col-md-12">
-                      <div class="panel form-element-padding">
-                        <div class="panel-heading">
-
-                        <h4>Spesifikasi Aplikasi</h4>
-                        </div>
-                         <div class="panel-body" style="padding-bottom:30px;">
-                          <div class="col-md-12">
-
-                          	<div class="form-group"><label class="col-sm-2 control-label text-right">Nama Aplikasi</label>
-                              <div class="col-sm-10"><input type="text" class="form-control"></div>
-                            </div>
-                       </div>
-                   </div>
-                </div>
-
-
-        <div class="form-group">
-            <div class="col-md-6 col-md-offset-4">
-                <button type="submit" class="btn btn-success">
-                    Submit
-                </button>
-            </div>
+               </div>
+              </form>
+          </div>
+          <a href="" class="btn btn-success btn-md" style="float:right" >Submit</a>
         </div>
+      </div>
+    </div>
+</div>
