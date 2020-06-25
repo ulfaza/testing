52d0bfd2646dfe45a7a9d18e95af57db13442b82
ulfaza <ulfatuzzahroh2@gmail.com>
2020-06-25 19:45:55 +0700

    coba pingdom api get

--- app/Http/Controllers/PSController.php
+++ app/Http/Controllers/PSController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
+use Ixudra\Curl\Facades\Curl;
 
 class PSController extends Controller
 {
@@ -15,14 +14,29 @@ class PSController extends Controller
 
     public function loadtest()
     {
-		$process = new Process(['C:\Program Files (x86)\Python38-32\python', 'C:\xampp\htdocs\testing\public\flask\main.py']);
-		$process->run();
-
-		// executes after the command finishes
-		if (!$process->isSuccessful()) {
-		    throw new ProcessFailedException($process);
-		}
-		echo $process->getOutput();
+	    // Init cURL
+	    $curl = curl_init();
+	    // Set target URL
+	    curl_setopt($curl, CURLOPT_URL, "https://api.pingdom.com/api/3.1/checks");
+	    // Set the desired HTTP method (GET is default, see the documentation for each request)
+	    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "GET");
+	    // Add header with Bearer Authorization
+	    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer TRG11avzEbCu6tA-VJYVgbRx3UGk42nacpoPyajPGUrJjfjJMvhR4qxW-NkG-C1IPKd7h4I"));
+	    // Ask cURL to return the result as a string
+	    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	    // Execute the request and decode the json result into an associative array
+	    $response = json_decode(curl_exec($curl), true);
+	    // Check for errors returned by the API
+	    if (isset($response['error'])) {
+	        print "Error: " . $response['error']['errormessage'] . "\n";
+	        exit;
+	    }
+	    // Fetch the list of checks from the response
+	    $checksList = $response['checks'];
+	    // Print the names and statuses of all checks in the list
+	    foreach ($checksList as $check) {
+	        print $check['name'] . " is " . $check['status'] . "\n";
+    	}
     }    
 }
 
