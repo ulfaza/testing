cead51fd25765af1a84eb8389b3824cd14e59498
zaayn <muzayyin937@gmail.com>
2020-12-23 08:24:41 +0700

    add migration, fix login, add .env.example

---
+++ database/migrations/2020_12_23_011435_create_subkarakteristik_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubkarakteristikTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subkarakteristik', function (Blueprint $table) {
+            $table->increments('sk_id')->unique();
+            $table->integer('k_id')->unsigned();
+            $table->string('sk_nama');
+            $table->decimal('bobot_relatif',8,2);
+            $table->decimal('bobot_absolut',8,2);
+            $table->decimal('nilai_subfaktor',8,2);
+            $table->decimal('nilai_absolut',8,2);
+        });
+        Schema::table('subkarakteristik', function($table){
+            $table->foreign('k_id')
+                ->references('k_id')
+                ->on('karakteristik')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subkarakteristik');
+    }
+}
