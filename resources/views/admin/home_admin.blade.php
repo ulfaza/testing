e3920f57b49dd258d4da3650bc47d672036aefe7
itsnadh <itsnadzakia@gmail.com>
2020-12-29 12:03:42 +0700

    homeey

--- resources/views/admin/home_admin.blade.php
+++ resources/views/admin/home_admin.blade.php
@@ -2,75 +2,19 @@
 @include('layouts.includes.admin_leftmenu')
 @section('tabeladmin')
 
-<style>
-#customers {
-  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 80%;
-  margin-left: 10%;
-  margin-bottom: 105px;
-}
-
-#customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#customers tr:nth-child(even){background-color: #f2f2f2;}
-
-#customers tr:hover {background-color: #ddd;}
-
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #5DADE2;
-  color: white;
-}
-
-</style>
-
 <div id="content">
-    <div class="panel">
-      <div class="panel-body">
-          <div class="col-md-6 col-sm-12">
-            <h3 class="animated fadeInLeft">WELCOME {{ Auth::user()->name }}</h3>
-            <p class="animated fadeInDown"><span class="fa  fa-map-marker"></span> Batavia,Indonesia</p>
-        </div>
-        <div class="col-md-6 col-sm-12">
-            <div class="col-md-6 col-sm-6 text-right" style="padding-left:10px;">
-              <h3 style="color:#DDDDDE;"><span class="fa  fa-map-marker"></span> Banyumas</h3>
-              <h1 style="margin-top: -10px;color: #ddd;">30<sup>o</sup></h1>
-            </div>
-            <div class="col-md-6 col-sm-6">
-               <div class="wheather">
-                <div class="stormy rainy animated pulse infinite">
-                  <div class="shadow">
-                    
-                  </div>
-                </div>
-                <div class="sub-wheather">
-                  <div class="thunder">
-                    
-                  </div>
-                  <div class="rain">
-                      <div class="droplet droplet1"></div>
-                      <div class="droplet droplet2"></div>
-                      <div class="droplet droplet3"></div>
-                      <div class="droplet droplet4"></div>
-                      <div class="droplet droplet5"></div>
-                      <div class="droplet droplet6"></div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel block">
+                    <div class="panel-body">
+                        <h3 class="animated fadeInLeft">Welcome {{ Auth::user()->name }}</h3>
                     </div>
                 </div>
-              </div>
-            </div>                   
+            </div>
         </div>
-      </div>                    
-    </div>
 
-    <div class="col-md-12" style="padding:20px;">
+    <div class="col-s-12" style="padding:20px;">
         <div class="col-md-12 padding-0">
-                
                 <div class="col-md-3">
                     <a href="{{asset('/admin/karakteristik')}}">
                         <div class="panel box-v1">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,82 @@
 @include('layouts.includes.header')
 @include('layouts.includes.leftmenu')
-
 @section('content')
+
+    <div id="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel block">
+                    <div class="panel-body">
+                        <h3 class="animated fadeInLeft">Welcome {{ Auth::user()->name }}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12" style="padding:20px;">
+            <div class="col-md-12 padding-0">
+                <div class="col-md-4">
+                    <a href="{{asset('/softwaretester/aplikasi')}}">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center">
+                            <h3>Aplikasi</h3>
+                            <hr/>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="{{asset('/softwaretester/bobot')}}">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center">
+                            <h3>Bobot Karakteristik</h3>
+                            <hr/>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="{{asset('/softwaretester/bobot')}}">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center">
+                            <h3>Bobot Karakteristik</h3>
+                            <hr/>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="{{asset('/softwaretester/bobot')}}">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center">
+                            <h3>Bobot Karakteristik</h3>
+                            <hr/>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="{{asset('/softwaretester/bobot')}}">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center">
+                            <h3>Bobot Karakteristik</h3>
+                            <hr/>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4">
+                    <a href="{{asset('/softwaretester/bobot')}}">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center">
+                            <h5>Bobot KarakteristikKarakteristikKarakteristikKarakteristikKarakteristikKarakteristikKarakteristikKarakteristik</h5>
+                            <hr/>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
