a0f1bf8717e5e1b4e74f1ea2c2d7a8d1ed46f85b
ulfaza <ulfatuzzahroh2@gmail.com>
2021-01-25 17:13:58 +0700

    edit nilai.blade

--- resources/views/nilai_app.blade.php
+++ resources/views/nilai_app.blade.php
@@ -116,22 +116,6 @@
                             <a href="{{route('capacity',$s->sk_id)}}" class="btn btn-warning btn-sm capacity">
                               <span class="fa fa-pencil"></span>
                             </a>
-                            <div class="col-md-3">
-                              <div class="sk-fading-circle">
-                                <div class="sk-circle1 sk-circle"></div>
-                                <div class="sk-circle2 sk-circle"></div>
-                                <div class="sk-circle3 sk-circle"></div>
-                                <div class="sk-circle4 sk-circle"></div>
-                                <div class="sk-circle5 sk-circle"></div>
-                                <div class="sk-circle6 sk-circle"></div>
-                                <div class="sk-circle7 sk-circle"></div>
-                                <div class="sk-circle8 sk-circle"></div>
-                                <div class="sk-circle9 sk-circle"></div>
-                                <div class="sk-circle10 sk-circle"></div>
-                                <div class="sk-circle11 sk-circle"></div>
-                                <div class="sk-circle12 sk-circle"></div>
-                              </div>
-                            </div>
                           </td>
                         @else
                           <td>
